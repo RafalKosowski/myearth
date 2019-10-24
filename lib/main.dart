37640f2c35fb934dd17aed7e33c4b3eb2d8d0282
czareks <czarek1gplay@gmail.com>
2019-10-24 16:00:47 +0200

    Dodane api???

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,356 @@
-import 'package:flutter/material.dart';
 import 'chart.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'dart:async';
 import 'dart:io';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+//API
+//stacje
+Future<stacje> fetchPost() async {
+  final response =
+  await http.get('http://api.gios.gov.pl/pjp-api/rest/station/findAll');
+  await http.get('http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/{stationId}');
+  await http.get('http://api.gios.gov.pl/pjp-api/rest/station/sensors/{stationId}');
+  await http.get('http://api.gios.gov.pl/pjp-api/rest/data/getData/{sensorId}');
+
+  if (response.statusCode == 200) {
+    // If the call to the server was successful, parse the JSON.
+    return stacje.fromJson(json.decode(response.body));
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to load post');
+  }
+
+}
+
+class stacje {
+  int id;
+  String stationName;
+  String gegrLat;
+  String gegrLon;
+  City city;
+  Null addressStreet;
+
+  stacje(
+      {this.id,
+        this.stationName,
+        this.gegrLat,
+        this.gegrLon,
+        this.city,
+        this.addressStreet});
+
+  stacje.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    stationName = json['stationName'];
+    gegrLat = json['gegrLat'];
+    gegrLon = json['gegrLon'];
+    city = json['city'] != null ? new City.fromJson(json['city']) : null;
+    addressStreet = json['addressStreet'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['stationName'] = this.stationName;
+    data['gegrLat'] = this.gegrLat;
+    data['gegrLon'] = this.gegrLon;
+    if (this.city != null) {
+      data['city'] = this.city.toJson();
+    }
+    data['addressStreet'] = this.addressStreet;
+    return data;
+  }
+}
+
+class City {
+  int id;
+  String name;
+  Commune commune;
+
+  City({this.id, this.name, this.commune});
+
+  City.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    commune =
+    json['commune'] != null ? new Commune.fromJson(json['commune']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    if (this.commune != null) {
+      data['commune'] = this.commune.toJson();
+    }
+    return data;
+  }
+}
+
+class Commune {
+  String communeName;
+  String districtName;
+  String provinceName;
+
+  Commune({this.communeName, this.districtName, this.provinceName});
+
+  Commune.fromJson(Map<String, dynamic> json) {
+    communeName = json['communeName'];
+    districtName = json['districtName'];
+    provinceName = json['provinceName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['communeName'] = this.communeName;
+    data['districtName'] = this.districtName;
+    data['provinceName'] = this.provinceName;
+    return data;
+  }
+}
+
+//index
+
+class index {
+  int id;
+  String stCalcDate;
+  StIndexLevel stIndexLevel;
+  String stSourceDataDate;
+  String so2CalcDate;
+  Null so2IndexLevel;
+  String so2SourceDataDate;
+  String no2CalcDate;
+  Null no2IndexLevel;
+  String no2SourceDataDate;
+  String coCalcDate;
+  Null coIndexLevel;
+  String coSourceDataDate;
+  String pm10CalcDate;
+  Null pm10IndexLevel;
+  String pm10SourceDataDate;
+  String pm25CalcDate;
+  Null pm25IndexLevel;
+  Null pm25SourceDataDate;
+  String o3CalcDate;
+  Null o3IndexLevel;
+  String o3SourceDataDate;
+  String c6h6CalcDate;
+  Null c6h6IndexLevel;
+  String c6h6SourceDataDate;
+
+  index(
+      {this.id,
+        this.stCalcDate,
+        this.stIndexLevel,
+        this.stSourceDataDate,
+        this.so2CalcDate,
+        this.so2IndexLevel,
+        this.so2SourceDataDate,
+        this.no2CalcDate,
+        this.no2IndexLevel,
+        this.no2SourceDataDate,
+        this.coCalcDate,
+        this.coIndexLevel,
+        this.coSourceDataDate,
+        this.pm10CalcDate,
+        this.pm10IndexLevel,
+        this.pm10SourceDataDate,
+        this.pm25CalcDate,
+        this.pm25IndexLevel,
+        this.pm25SourceDataDate,
+        this.o3CalcDate,
+        this.o3IndexLevel,
+        this.o3SourceDataDate,
+        this.c6h6CalcDate,
+        this.c6h6IndexLevel,
+        this.c6h6SourceDataDate});
+
+  index.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    stCalcDate = json['stCalcDate'];
+    stIndexLevel = json['stIndexLevel'] != null
+        ? new StIndexLevel.fromJson(json['stIndexLevel'])
+        : null;
+    stSourceDataDate = json['stSourceDataDate'];
+    so2CalcDate = json['so2CalcDate'];
+    so2IndexLevel = json['so2IndexLevel'];
+    so2SourceDataDate = json['so2SourceDataDate'];
+    no2CalcDate = json['no2CalcDate'];
+    no2IndexLevel = json['no2IndexLevel'];
+    no2SourceDataDate = json['no2SourceDataDate'];
+    coCalcDate = json['coCalcDate'];
+    coIndexLevel = json['coIndexLevel'];
+    coSourceDataDate = json['coSourceDataDate'];
+    pm10CalcDate = json['pm10CalcDate'];
+    pm10IndexLevel = json['pm10IndexLevel'];
+    pm10SourceDataDate = json['pm10SourceDataDate'];
+    pm25CalcDate = json['pm25CalcDate'];
+    pm25IndexLevel = json['pm25IndexLevel'];
+    pm25SourceDataDate = json['pm25SourceDataDate'];
+    o3CalcDate = json['o3CalcDate'];
+    o3IndexLevel = json['o3IndexLevel'];
+    o3SourceDataDate = json['o3SourceDataDate'];
+    c6h6CalcDate = json['c6h6CalcDate'];
+    c6h6IndexLevel = json['c6h6IndexLevel'];
+    c6h6SourceDataDate = json['c6h6SourceDataDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['stCalcDate'] = this.stCalcDate;
+    if (this.stIndexLevel != null) {
+      data['stIndexLevel'] = this.stIndexLevel.toJson();
+    }
+    data['stSourceDataDate'] = this.stSourceDataDate;
+    data['so2CalcDate'] = this.so2CalcDate;
+    data['so2IndexLevel'] = this.so2IndexLevel;
+    data['so2SourceDataDate'] = this.so2SourceDataDate;
+    data['no2CalcDate'] = this.no2CalcDate;
+    data['no2IndexLevel'] = this.no2IndexLevel;
+    data['no2SourceDataDate'] = this.no2SourceDataDate;
+    data['coCalcDate'] = this.coCalcDate;
+    data['coIndexLevel'] = this.coIndexLevel;
+    data['coSourceDataDate'] = this.coSourceDataDate;
+    data['pm10CalcDate'] = this.pm10CalcDate;
+    data['pm10IndexLevel'] = this.pm10IndexLevel;
+    data['pm10SourceDataDate'] = this.pm10SourceDataDate;
+    data['pm25CalcDate'] = this.pm25CalcDate;
+    data['pm25IndexLevel'] = this.pm25IndexLevel;
+    data['pm25SourceDataDate'] = this.pm25SourceDataDate;
+    data['o3CalcDate'] = this.o3CalcDate;
+    data['o3IndexLevel'] = this.o3IndexLevel;
+    data['o3SourceDataDate'] = this.o3SourceDataDate;
+    data['c6h6CalcDate'] = this.c6h6CalcDate;
+    data['c6h6IndexLevel'] = this.c6h6IndexLevel;
+    data['c6h6SourceDataDate'] = this.c6h6SourceDataDate;
+    return data;
+  }
+}
+
+class StIndexLevel {
+  int id;
+  String indexLevelName;
+
+  StIndexLevel({this.id, this.indexLevelName});
+
+  StIndexLevel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    indexLevelName = json['indexLevelName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['indexLevelName'] = this.indexLevelName;
+    return data;
+  }
+}
+
+//sensory
+
+class sensory {
+  int id;
+  int stationId;
+  Param param;
+
+  sensory({this.id, this.stationId, this.param});
+
+  sensory.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    stationId = json['stationId'];
+    param = json['param'] != null ? new Param.fromJson(json['param']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['stationId'] = this.stationId;
+    if (this.param != null) {
+      data['param'] = this.param.toJson();
+    }
+    return data;
+  }
+}
+
+class Param {
+  String paramName;
+  String paramFormula;
+  String paramCode;
+  int idParam;
+
+  Param({this.paramName, this.paramFormula, this.paramCode, this.idParam});
+
+  Param.fromJson(Map<String, dynamic> json) {
+    paramName = json['paramName'];
+    paramFormula = json['paramFormula'];
+    paramCode = json['paramCode'];
+    idParam = json['idParam'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['paramName'] = this.paramName;
+    data['paramFormula'] = this.paramFormula;
+    data['paramCode'] = this.paramCode;
+    data['idParam'] = this.idParam;
+    return data;
+  }
+}
+
+//dane
+
+class dane {
+  String key;
+  List<Values> values;
+
+  dane({this.key, this.values});
+
+  dane.fromJson(Map<String, dynamic> json) {
+    key = json['key'];
+    if (json['values'] != null) {
+      values = new List<Values>();
+      json['values'].forEach((v) {
+        values.add(new Values.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['key'] = this.key;
+    if (this.values != null) {
+      data['values'] = this.values.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Values {
+  String date;
+  double value;
+
+  Values({this.date, this.value});
+
+  Values.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    value = json['value'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['date'] = this.date;
+    data['value'] = this.value;
+    return data;
+  }
+}
+//END API
+
+
 
 void main() => runApp(MyApp());
 
@@ -386,9 +732,10 @@ class NewPage4 extends StatelessWidget {
       body: Center(
         child: ListView(
           children: <Widget>[
-           Text("Testowanie lokalizacji. (W przyszłości ziemia 3d pokazująca zmiany klimatyczne na ziemi)"),
+           Text('Testowanie lokalizacji. (W przyszłości ziemia 3d pokazująca zmiany klimatyczne na ziemi)',
+            style: TextStyle(fontSize: 21)),
           Text('Location: Lat${userLocation?.latitude}, Long: ${userLocation?.longitude}',
-          style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             ],
         ),),
     );
